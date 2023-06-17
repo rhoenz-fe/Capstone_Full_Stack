792233b78780f887023545e6b8f04383e40813e6
Rhoenzfe <rhoenz.fermin@gmail.com>
2023-06-17 18:38:54 -0400

    one page

--- part3/phonebook2/index.js
+++ part3/phonebook2/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('data', (request) => {
   return JSON.stringify(request.body)
