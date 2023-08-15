6ae5d0ab48a45b6eaa21609b7a7baaf6d66c64d0
Rhoenzfe <102177713+rhoenz-fe@users.noreply.github.com>
2023-08-15 14:51:26 -0400

    Delete part_4/blog directory

--- part_4/blog/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const config = require('./util/config')
-const logger = require('./util/logger')
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
